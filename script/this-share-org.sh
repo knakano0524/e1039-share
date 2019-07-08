bd172255a77920d7b98b157e6b4c781b5c982568
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2019-07-08 08:03:04 -0500

    I added a setting about PYTHIA8 to this-share.sh.

--- script/this-share-org.sh
+++ script/this-share-org.sh
@@ -9,6 +9,9 @@ FN_GEANT4=$E1039_SHARE/bin/geant4.sh
 test -e $FN_GEANT4 && source $FN_GEANT4
 unset FN_GEANT4
 
+export     PYTHIA8=$E1039_SHARE/share/Pythia8
+export PYTHIA8DATA=$PYTHIA8/xmldoc
+
 export            PATH=$E1039_SHARE/bin:$PATH
 export           CPATH=$E1039_SHARE/include:$CPATH
 export    LIBRARY_PATH=$E1039_SHARE/lib:$E1039_SHARE/lib64:$LIBRARY_PATH
