31764709446e471bea76fb4c8aff57db40f01a17
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2019-12-10 23:58:52 -0600

    The setup script (`this-share.sh`) was updated.  It now cleans up the environmental variables when E1039_SHARE is already defined.

--- script/this-share-org.sh
+++ script/this-share-org.sh
@@ -1,3 +1,11 @@
+
+if [ $E1039_SHARE ] ; then # Clean up the old components
+    PATH=${PATH//"$E1039_SHARE/bin:"}
+    CPATH=${CPATH//"$E1039_SHARE/include:"}
+    LIBRARY_PATH=${LIBRARY_PATH//"$E1039_SHARE/lib:$E1039_SHARE/lib64:"}
+    LD_LIBRARY_PATH=${LD_LIBRARY_PATH//"$E1039_SHARE/lib:$E1039_SHARE/lib64:"}
+fi
+
 export E1039_SHARE=$(dirname $(readlink -f $BASH_SOURCE))
 export LC_ALL=C
 
@@ -15,9 +23,10 @@ unset FN_GEANT4
 export     PYTHIA8=$E1039_SHARE/share/Pythia8
 export PYTHIA8DATA=$PYTHIA8/xmldoc
 
-export            PATH=$E1039_SHARE/bin:$PATH
-export           CPATH=$E1039_SHARE/include:$CPATH
-export    LIBRARY_PATH=$E1039_SHARE/lib:$E1039_SHARE/lib64:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$E1039_SHARE/lib:$E1039_SHARE/lib64:$LD_LIBRARY_PATH
+export              PATH=$E1039_SHARE/bin:$PATH
+export             CPATH=$E1039_SHARE/include:$CPATH
+export      LIBRARY_PATH=$E1039_SHARE/lib:$E1039_SHARE/lib64:$LIBRARY_PATH
+export   LD_LIBRARY_PATH=$E1039_SHARE/lib:$E1039_SHARE/lib64:$LD_LIBRARY_PATH
+export ROOT_INCLUDE_PATH=$E1039_SHARE/include/GenFit
 
-export ROOT_INCLUDE_PATH=$E1039_SHARE/include/GenFit:$E1039_SHARE/include/mysql:$ROOT_INCLUDE_PATH
+export ROOT_INCLUDE_PATH_E1039_SHARE=$ROOT_INCLUDE_PATH
