dfe1bd4531539b1295457a4dd38ae84f259e9183
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2021-02-26 07:55:15 -0600

    Modified the patch command because it stopped working in Singularity...

--- script/sub/30_pythia6.sh
+++ script/sub/30_pythia6.sh
@@ -14,14 +14,15 @@ mkdir -p $DIR_WORK
 cd       $DIR_WORK
 
 wget --quiet http://home.fnal.gov/~rhatcher/build_pythia6.sh.txt
-mv build_pythia6.sh.txt build_pythia6.sh
+patch -o build_pythia6.sh build_pythia6.sh.txt $DIR_DAT/patch.txt
 chmod 775 build_pythia6.sh
-
-patch <$DIR_DAT/patch.txt
-
 ./build_pythia6.sh 6.4.28
 cp -a v6_428/lib/libPythia6.so $DIR_INST/lib
 
+## Note on 'patch'
+## The past command "patch <$DIR_DAT/patch.txt" stopped working in Singularity
+## due to this error; patch: **** Failed to set the owner of file ./build_pythia6.sh.o7ZxYPO : Invalid argument
+## Strangely it worked fine when I first tested...
 
 ## libPythia6.so could be created by the self-compilation procedure below but
 ## failed at being used in the ROOT compilation.
